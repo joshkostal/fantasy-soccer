198084a62cfa62ceb5a5efe412e5c7696ccdb91f
Josh Kostal <joshkostal4@gmail.com>
2021-07-31 22:08:57 -0400

    Added bench and style updates to Team page

--- client/src/components/Roster.tsx
+++ client/src/components/Roster.tsx
@@ -1,4 +1,13 @@
-import { IonCol, IonContent, IonGrid, IonRow } from "@ionic/react";
+import {
+  IonCard,
+  IonCardContent,
+  IonCardHeader,
+  IonCardTitle,
+  IonCol,
+  IonContent,
+  IonGrid,
+  IonRow,
+} from "@ionic/react";
 import { EPosition, Player, PositionClassMapping } from "../models/team";
 import "../styles/Team.css";
 import "../styles/index.css";
@@ -9,36 +18,50 @@ interface RosterProps {
 }
 
 const Roster: React.FC<RosterProps> = ({ roster }: RosterProps) => {
-  const rosterTable = roster
-    .filter((player) => player.isStarter)
-    .map((player) => (
-      <IonRow key={player.id}>
-        <IonCol
-          size="1.5"
-          className={`light-title position-tile ${
-            PositionClassMapping[player.positionId as EPosition]
-          }`}
-        >
-          {player.position}
-        </IonCol>
-        <IonCol size="5" className="light-title player-name-col">
-          {player.name}
-        </IonCol>
-        <IonCol size="4.5" className="light-title">
-          {player.isHome ? "" : "at "}
-          {player.opponent}
-        </IonCol>
-        <IonCol size="1" className="ion-text-right light-title">
-          {player.points}
-        </IonCol>
-      </IonRow>
-    ));
+  const rosterTable = (isStarter: boolean) =>
+    roster
+      .filter((player) => player.isStarter == isStarter)
+      .map((player) => (
+        <IonRow key={player.id}>
+          <IonCol
+            size="1.5"
+            className={`light-title position-tile ${
+              PositionClassMapping[player.positionId as EPosition]
+            }`}
+          >
+            {player.position}
+          </IonCol>
+          <IonCol size="6" className="light-title player-name-col">
+            {player.name}
+          </IonCol>
+          <IonCol size="3.5" className="light-title">
+            {!player.isHome && "@"}
+            {player.opponent}
+          </IonCol>
+          <IonCol size="1" className="ion-text-right light-title">
+            {player.points}
+          </IonCol>
+        </IonRow>
+      ));
 
   return (
-    <IonContent color="background">
-      <IonGrid className="roster-card" color="light">
-        {rosterTable}
-      </IonGrid>
+    <IonContent>
+      <IonCard>
+        <IonCardHeader>
+          <IonCardTitle className="light-title">Starters</IonCardTitle>
+        </IonCardHeader>
+        <IonCardContent>
+          <IonGrid>{rosterTable(true)}</IonGrid>
+        </IonCardContent>
+      </IonCard>
+      <IonCard>
+        <IonCardHeader>
+          <IonCardTitle className="light-title">Bench</IonCardTitle>
+        </IonCardHeader>
+        <IonCardContent>
+          <IonGrid>{rosterTable(false)}</IonGrid>
+        </IonCardContent>
+      </IonCard>
     </IonContent>
   );
 };
--- client/src/models/team.ts
+++ client/src/models/team.ts
@@ -30,3 +30,28 @@ export const PositionClassMapping: Record<EPosition, string> = {
   [EPosition.CB]: "centerback",
   [EPosition.GK]: "goal-keeper",
 };
+
+/*
+PL Team Abbreviations:
+
+Arsenal - AFC
+Aston Villa - AVFC
+Brighton - BHAFC
+Burnley - BFC
+Chelsea - CFC
+Crystal Palace - CPFC
+Everton - EFC
+Fulham - FFC
+Leeds - LUFC
+Leicester City - LCFC
+Liverpool - LFC
+Man City - MCFC
+Man U - MUFC
+Newcastle - NUFC
+Southampton - SFC
+Sheffield United - SUFC
+Tottenham - THFC
+West Brom - WBAFC
+West Ham - WHUFC
+Wolverhampton - WWFC
+*/
