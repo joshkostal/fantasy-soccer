9a2823d183d2b58c35ae1577e3a8a6f63bc8aff7
Josh Kostal <joshkostal4@gmail.com>
2022-03-11 20:49:15 -0500

    Override connection string when running server

--- server/src/index.ts
+++ server/src/index.ts
@@ -4,8 +4,19 @@ import resolvers from "./graphql/resolvers";
 import cors from "cors";
 import { PrismaClient } from "@josh-kostal/prisma/lib/client";
 
+const getConnectionString = (): string => {
+  const config = require('../local_db.json');
+  return config['connectionString'];
+};
+
 const options: Options = { port: 4000 };
-const prisma = new PrismaClient();
+const prisma = new PrismaClient({
+  datasources: {
+    db: {
+      url: getConnectionString(),
+    },
+  },
+});
 
 const server = new GraphQLServer({
   typeDefs,
